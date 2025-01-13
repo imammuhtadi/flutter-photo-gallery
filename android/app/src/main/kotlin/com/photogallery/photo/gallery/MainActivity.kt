fb1be0ed1d87d27e6218f6f914aae8de53253af2
Imam Muhtadi <muhtadiimam05@gmail.com>
2025-01-13 23:51:06 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/photogallery/photo/gallery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.photogallery.photo_gallery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:photo_gallery/app/app.dart';
+import 'package:photo_gallery/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
